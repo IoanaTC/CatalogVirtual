ce4f9f03ab94af3c662c4949141a9457aa3fe1d4
IoanaTC <ioanateodoraconstantin@yahoo.com>
2024-01-16 19:55:51 +0200

    [init] modify init

--- CatalogVirtual/init.bat
+++ CatalogVirtual/init.bat
@@ -1,11 +1,10 @@
 @echo off
 
-npm install -g npm
-
 npm init -y
 
 npm install express apollo-server-express graphql mysql2 jsonwebtoken sequelize dotenv mariadb sequelize-cli faker @ngneat/falso casual
 
+npx sequelize-cli db:migrate
 npx sequelize-cli db:seed:all
 
 node index.js
